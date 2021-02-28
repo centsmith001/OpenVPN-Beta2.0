03fcb266f8114e3dcb07dd76176057c9467b93b0
centsmith001 <79389709+centsmith001@users.noreply.github.com>
2021-02-28 23:54:15 +0800

    Update OpenVPN-Beta2.0.sh

--- OpenVPN-Beta2.0.sh
+++ OpenVPN-Beta2.0.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt-get update && apt-get upgrade -y
-rm OpenVPN-Beta.sh
  # First thing to do is check if this machine is Debian
  source /etc/os-release
 if [[ "$ID" != 'debian' ]]; then
