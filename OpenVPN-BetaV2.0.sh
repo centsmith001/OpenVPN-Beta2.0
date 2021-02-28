7746481581505b3594d514355053999d7367a6b9
centsmith001 <79389709+centsmith001@users.noreply.github.com>
2021-03-01 01:05:02 +0800

    Update OpenVPN-BetaV2.0.sh

--- OpenVPN-BetaV2.0.sh
+++ OpenVPN-BetaV2.0.sh
@@ -4,7 +4,7 @@ rm -Rf OpenVPN-Beta2.0.sh
 # First thing to do is check if this machine is Debian
 source /etc/os-release
 if [[ "$ID" != 'debian' ]]; then
-echo -e OS not supported, exting...
+echo -e "OS not supported, exting..."
 exit 1
 fi
 #Some workaround for OpenVZ machines for "Startup error" openvpn service
